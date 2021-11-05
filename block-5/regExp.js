238d54e59909f0e1470144e9bf190c9ad3041cd6
Stanislav-Karasyk <99.kara.99@gmail.com>
2021-11-05 10:17:00 +0200

    fix 2

--- block-5/regExp.js
+++ block-5/regExp.js
@@ -1,35 +1,11 @@
-const phoneNumber = "+38(066)333-22-22";
-
 const regexpPhoneNumber = /^\+\d{2}\(\d{3}\)\d{3}-\d{2}-\d{2}$/;
-
-// console.log(regexpPhoneNumber.test(phoneNumber));
-
-// ==========
-
-const email = "test.test@mega.zik.dj";
 
 const regexpEmail =
   /^[a-zA-Z0-9]+(\.|-)?[a-zA-Z0-9]+@([a-zA-Z]+\.?[a-zA-Z])+\.[a-zA-Z]{2,6}$/;
 
-// console.log(regexpEmail.test(email));
-
-// ==========
-const site = "http://test-test.dev";
-
 const regexpSite = /^https?:\/\/[a-zA-Z0-9]+-?[a-zA-Z0-9]+\.[a-zA-Z]{2,6}$/;
-
-// console.log(regexpSite.test(site));
-
-// ==========
-const password = "abc_123";
 
 const regexpPassword = /^[a-z0-9_]{6,25}$/;
 
-// console.log(regexpPassword.test(password));
-
-// ==========
-const ipv4 = "255.255.255.255";
-
 const regexpIpv4 = /^((\d{1}|\d{2}|[0-2][0-4]\d|[0-2][0-5][0-5])\.){3}((\d{1}|\d{2}|[0-2][0-4]\d|[0-2][0-5][0-5]))$/;
 
-// console.log(regexpIpv4.test(ipv4));
