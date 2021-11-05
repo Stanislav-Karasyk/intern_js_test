7ea0a899fc7cbf191ba9adae83f19d72b2405dea
Stanislav-Karasyk <99.kara.99@gmail.com>
2021-11-05 10:21:35 +0200

    fix 4

--- block-5/regExp.js
+++ block-5/regExp.js
@@ -1,11 +1,9 @@
 const regexpPhoneNumber = /^\+\d{2}\(\d{3}\)\d{3}-\d{2}-\d{2}$/;
 
-const regexpEmail =
-  /^[a-zA-Z0-9]+(\.|-)?[a-zA-Z0-9]+@([a-zA-Z]+\.?[a-zA-Z])+\.[a-zA-Z]{2,6}$/;
+const regexpEmail = /^[a-zA-Z0-9]+(\.|-)?[a-zA-Z0-9]+@([a-zA-Z]+\.?[a-zA-Z])+\.[a-zA-Z]{2,6}$/;
 
 const regexpSite = /^https?:\/\/[a-zA-Z0-9]+-?[a-zA-Z0-9]+\.[a-zA-Z]{2,6}$/;
 
 const regexpPassword = /^[a-z0-9_]{6,25}$/;
 
-const regexpIpv4 =
-  /^((\d{1}|\d{2}|[0-2][0-4]\d|[0-2][0-5][0-5])\.){3}((\d{1}|\d{2}|[0-2][0-4]\d|[0-2][0-5][0-5]))$/;
+const regexpIpv4 = /^((\d{1}|\d{2}|[0-2][0-4]\d|[0-2][0-5][0-5])\.){3}((\d{1}|\d{2}|[0-2][0-4]\d|[0-2][0-5][0-5]))$/;
